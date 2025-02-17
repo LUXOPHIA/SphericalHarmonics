af29e9f10f468c47430feba468ca8e7546bc9c90
luxidea <contact@luxidea.net>
2025-02-17 16:16:24 +0900

    TSPHarmonics3D クラスを追加。

--- _LIBRARY/LUXOPHIA/LUX.SphericalHarmonics/FMX/LUX.SH.FMX.pas
+++ _LIBRARY/LUXOPHIA/LUX.SphericalHarmonics/FMX/LUX.SH.FMX.pas
@@ -2,17 +2,66 @@
 
 interface //#################################################################### ■
 
-uses LUX;
+uses System.SysUtils, System.RTLConsts, System.Classes,
+     System.Types, System.UITypes, System.Math.Vectors,
+     FMX.Types3D, FMX.Controls3D, FMX.MaterialSources,
+     LUX,
+     LUX.D2,
+     LUX.D3,
+     LUX.D4x4,
+     LUX.D1.Diff,
+     LUX.D2.Diff,
+     LUX.D3.Diff,
+     LUX.Complex.Diff,
+     LUX.FMX.Graphics.D3,
+     LUX.FMX.Graphics.D3.Shaper,
+     LUX.SH.Diff;
 
-//type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {RECORD}
-
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {CLASS}
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSPHarmonics3D
+
+     TSPHarmonics3D = class( TF3DShaper )
+     private
+       ///// M E T H O D
+       function XYtoI( const X_,Y_:Integer ) :Integer; inline;
+     protected
+       _Polygons :TMeshData;
+       _Material :TLightMaterialSource;
+       _DivX     :Integer;
+       _DivY     :Integer;
+       _SPHarm   :TdSPHarmonics;
+       _N        :Integer;
+       _M        :Integer;
+       ///// A C C E S S O R
+       procedure SetDivX( const DivX_:Integer );
+       procedure SetDivY( const DivY_:Integer );
+       function GetSPHarm :TdSPHarmonics;
+       procedure SetSPHarm( const SPHarm_:TdSPHarmonics );
+       function GetN :Integer;
+       procedure SetN( const N_:Integer );
+       function GetM :Integer;
+       procedure SetM( const M_:Integer );
+       ///// M E T H O D
+       procedure Render; override;
+       function MeshVertexs( const T_:TdDouble2D ) :TdDouble3D;
+       procedure MakeGeometry; override;
+       procedure MakeTopology; override;
+     public
+       constructor Create( Owner_:TComponent ); override;
+       destructor Destroy; override;
+       ///// P R O P E R T Y
+       property Material :TLightMaterialSource read   _Material write   _Material;
+       property DivX     :Integer              read   _DivX     write SetDivX    ;
+       property DivY     :Integer              read   _DivY     write SetDivY    ;
+       property SPHarm   :TdSPHarmonics        read GetSPHarm   write SetSPHarm  ;
+       property N        :Integer              read GetN        write SetN       ;
+       property M        :Integer              read GetM        write SetM       ;
+     end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
@@ -20,21 +69,260 @@ implementation //###############################################################
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {RECORD}
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {CLASS}
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSPHarmonics3D
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+function TSPHarmonics3D.XYtoI( const X_,Y_:Integer ) :Integer;
+begin
+     Result := ( _DivX + 1 ) * Y_ + X_;
+end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
 
+//////////////////////////////////////////////////////////////// A C C E S S O R
+
+procedure TSPHarmonics3D.SetDivX( const DivX_:Integer );
+begin
+     if _DivX = DivX_ then Exit;
+
+     _DivX := DivX_;
+
+     upGeometry := True;
+     upTopology := True;
+
+     Repaint;
+end;
+
+procedure TSPHarmonics3D.SetDivY( const DivY_:Integer );
+begin
+     if _DivY = DivY_ then Exit;
+
+     _DivY := DivY_;
+
+     upGeometry := True;
+     upTopology := True;
+
+     Repaint;
+end;
+
+//------------------------------------------------------------------------------
+
+function TSPHarmonics3D.GetSPHarm :TdSPHarmonics;
+begin
+     Result := _SPHarm;
+end;
+
+procedure TSPHarmonics3D.SetSPHarm( const SPHarm_:TdSPHarmonics );
+begin
+     if _SPHarm = SPHarm_ then Exit;
+
+     _SPHarm := SPHarm_;
+
+     upGeometry := True;
+     upTopology := True;
+
+     Repaint;
+end;
+
+//------------------------------------------------------------------------------
+
+function TSPHarmonics3D.GetN :Integer;
+begin
+     Result := _N;
+end;
+
+procedure TSPHarmonics3D.SetN( const N_:Integer );
+begin
+     _N := N_;
+
+     upGeometry := True;
+     upTopology := True;
+
+     Repaint;
+end;
+
+function TSPHarmonics3D.GetM :Integer;
+begin
+     Result := _M;
+end;
+
+procedure TSPHarmonics3D.SetM( const M_:Integer );
+begin
+     _M := M_;
+
+     upGeometry := True;
+     upTopology := True;
+
+     Repaint;
+end;
+
+//////////////////////////////////////////////////////////////////// M E T H O D
+
+procedure TSPHarmonics3D.Render;
+begin
+     inherited;
+
+     _Polygons.Render( Context, TMaterialSource.ValidMaterial( _Material ), AbsoluteOpacity );
+end;
+
+function TSPHarmonics3D.MeshVertexs( const T_:TdDouble2D ) :TdDouble3D;
+var
+   H :TdDoubleC;
+   L, R :TdDouble;
+begin
+     SPHarm.AngleY := T_.Y;
+     SPHarm.AngleX := T_.X;
+
+     H := SPHarm[ N, M ];
+
+     L := LUX.D1.Diff.Abs( H.R ) * 10;
+
+     Result.Y := L * LUX.D1.Diff.Cos( T_.Y );
+            R := L * LUX.D1.Diff.Sin( T_.Y );
+     Result.X := +R * LUX.D1.Diff.Cos( T_.X );
+     Result.Z := -R * LUX.D1.Diff.Sin( T_.X );
+end;
+
+procedure TSPHarmonics3D.MakeGeometry;
+var
+   X, Y, I :Integer;
+   T :TdDouble2D;
+   P, VX, VY, NV :TDouble3D;
+begin
+     SPHarm.DegN := N;
+
+     with _Polygons.VertexBuffer do
+     begin
+          if not Assigned( SPHarm ) then
+          begin
+               Length := 0;  Exit;
+          end;
+
+          Length := ( _DivX + 1 ) * ( _DivY + 1 );
+
+          Y := 0;  T.Y := 0;
+          for X := 0 to _DivX do
+          begin
+               T.X := Pi2 / _DivX * X;
+
+               P := MeshVertexs( T ).o;
+
+               I := XYtoI( X, Y );
+
+               Vertices [ I ] := P;
+               Normals  [ I ] := P.Unitor;
+               TexCoord0[ I ] := TPointF.Create( X / _DivX, Y / _DivY );
+          end;
+
+          for Y := 1 to _DivY-1 do
+          begin
+               T.Y.o := Pi / _DivY * Y;
+
+               for X := 0 to _DivX do
+               begin
+                    T.X.o := Pi2 / _DivX * X;
+
+                    T.d := 0;
+
+                    P := MeshVertexs( T ).o;
+
+                    T.d := TDouble2D.Create( Pi2 / _DivX, 0 );
+
+                    VX := MeshVertexs( T ).d;
+
+                    T.d := TDouble2D.Create( 0, Pi / _DivY );
+
+                    VY := MeshVertexs( T ).d;
+
+                    NV := CrossProduct( VY, VX ).Unitor;
+
+                    I := XYtoI( X, Y );
+
+                    Vertices [ I ] := P;
+                    Normals  [ I ] := NV;
+                    TexCoord0[ I ] := TPointF.Create( X / _DivX, Y / _DivY );
+               end;
+          end;
+
+          Y := _DivY;  T.Y := Pi;
+          for X := 0 to _DivX do
+          begin
+               T.X := Pi2 / _DivX * X;
+
+               P := MeshVertexs( T ).o;
+
+               I := XYtoI( X, Y );
+
+               Vertices [ I ] := P;
+               Normals  [ I ] := P.Unitor;
+               TexCoord0[ I ] := TPointF.Create( X / _DivX, Y / _DivY );
+          end;
+     end;
+end;
+
+procedure TSPHarmonics3D.MakeTopology;
+var
+   X, Y, I :Integer;
+begin
+     with _Polygons.IndexBuffer do
+     begin
+          if not Assigned( SPHarm ) then
+          begin
+               Length := 0;  Exit;
+          end;
+
+          Length := 3{Poin} * 2{Face} * _DivX * _DivY;
+
+          I := 0;
+          for Y := 0 to _DivY-1 do
+          begin
+               for X := 0 to _DivX-1 do
+               begin
+                    //     X0     X1
+                    //  Y0 +------+
+                    //     |＼    |
+                    //     |  ＼  |
+                    //     |    ＼|
+                    //  Y1 +------+
+
+                    Indices[ I ] := XYtoI( X  , Y   );  Inc( I );
+                    Indices[ I ] := XYtoI( X+1, Y   );  Inc( I );
+                    Indices[ I ] := XYtoI( X+1, Y+1 );  Inc( I );
+
+                    Indices[ I ] := XYtoI( X+1, Y+1 );  Inc( I );
+                    Indices[ I ] := XYtoI( X  , Y+1 );  Inc( I );
+                    Indices[ I ] := XYtoI( X  , Y   );  Inc( I );
+               end;
+          end;
+     end;
+end;
+
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TSPHarmonics3D.Create( Owner_:TComponent );
+begin
+     inherited;
+
+     _Polygons := TMeshData.Create;
+
+     _Material := TLightMaterialSource.Create( Self );
+     _Material.Specular := TAlphaColors.Null;
+
+     DivX := 360;
+     DivY := 180;
+     N    := 0;
+     M    := 0;
+end;
+
+destructor TSPHarmonics3D.Destroy;
+begin
+     _Polygons.Free;
+
+     inherited;
+end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
